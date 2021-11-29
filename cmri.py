93d77fa2388b53a8487510a57c729b587a3ee456
Kelly Loyd <kelly.loyd@icloud.com>
2021-11-29 07:50:59 -0600

    Init node work done.

--- cmri.py
+++ cmri.py
@@ -46,63 +46,66 @@ class CMRI:
         # iNumInputBytes = number of input bytes in the buffer
         # ns = number of 4 card sets or number of searchlights
         # MAXTRIES = Maximum number of PC tries to read input bytes prior to PC aborting inputs
-        pass
-        MT = ord('I')  # ASC("I")               'Define message type = "I" (decimal 73)
+        # pass
+        message_type = ord('I')  # ASC("I")               'Define message type = "I" (decimal 73)
         self.out_byte[1] = ord(node_type)     #ASC(NDP$)           #Define node definition parameter
-        self.out_byte[2] = INT(DL / 256)       #Set USIC delay high-order byte
-        self.out_byte[3] = dl - (OB(2) * 256)  #Set USIC delay low-order byte
+        self.out_byte[2] = int(dl / 256)       #Set USIC delay high-order byte
+        self.out_byte[3] = dl % 256            #Set USIC delay low-order byte
         self.out_byte[4] = ns                  #Define number of card sets of 4 for...
                                  #USIC and SUSIC cases and the...
                                  #...number of 2-lead yellow aspect...
                                  #...oscillating signals for the SMINI.
         lm = 4  #Initialize length of message to start of loading CT elements
 
-  #**CHECK TYPE OF NODE TO CONTINUE SPECIFIC INITIALIZATION
-     IF NDP$ = "M" THEN GOTO INITSMINI  #SMINI node so branch accordingly
+        #**CHECK TYPE OF NODE TO CONTINUE SPECIFIC INITIALIZATION
+        #IF NDP$ = "M" THEN GOTO INITSMINI  #SMINI node so branch accordingly
+        if node_type == "M":
+            # init SMINI - only send CT if NS > 0
+            # Always 6 configuration bytes in CT for SMINI
+            if ns > 0:
+                for i in range(1, 7):
+                    lm = lm + 1
+                    self.out_byte[lm] = ct[i]
+        else:
+            #init SUSIC
+            # ns is number of 4 card sets in the IOMBX
+            #INITUSIC:
+            #**SUSIC-NODE (either "N" or "X") SO LOAD CT( ) ARRAY ELEMENTS
+            #FOR I = 1 TO NS     #Loop through number of card sets...
+            #             #LM = LM + 1      #...accumulating message length while...
+            #OB(LM) = CT(I)   #...loading card type definition CT...
+            #NEXT I              #...array elements into output byte array
+            #GOTO TXMSG  #CT( )s complete so branch to transmit initialization...
+                             #...message to interface
+            for i in range(1, ns + 1):
+                lm = lm + 1
+                self.out_byte[lm] = ct[i]
 
-INITUSIC:
-  #**SUSIC-NODE (either "N" or "X") SO LOAD CT( ) ARRAY ELEMENTS
-     FOR I = 1 TO NS     #Loop through number of card sets...
-        LM = LM + 1      #...accumulating message length while...
-        OB(LM) = CT(I)   #...loading card type definition CT...
-     NEXT I              #...array elements into output byte array
-     GOTO TXMSG  #CT( )s complete so branch to transmit initialization...
-                 #...message to interface
-INITSMINI:
-  #**SMINI-NODE ("M") SO CHECK IF REQUIRES 2-LEAD OSCILLATION...
-                   #...SEARCHLIGHT SIGNALS
-     IF NS = 0 THEN GOTO TXMSG #No signals so hold message length at...
-                               #...LM = 4 and branch to transmit packet
+        #
+        self.txpack(node_number=ua, message_type=message_type, message_length=lm)
+        # clean up output bytes
+        #**COMPLETED USE OF OUTPUT BYTE ARRAY SO CLEAR IT BEFORE EXIT SUBROUTINE
+        for i in range(1, num_out_bytes + 1):
+            self.out_byte[i] = 0
 
-  #**SMINI CASE WITH SIGNALS (NS > 0) SO LOOP THROUGH TO LOAD...
-     FOR I = 1 TO 6       #...signal location CT array elements...
-        LM = LM + 1       #...into output byte array while...
-        OB(LM) = CT(I)    #...accumulating message length
-     NEXT I
 
-  #**FORM INITIALIZATION PACKET AND TRANSMIT TO INTERFACE
-TXMSG: CALL TXPACK      #Invoke transmit packet subroutine
-
-#**COMPLETED USE OF OUTPUT BYTE ARRAY SO CLEAR IT BEFORE EXIT SUBROUTINE
-     FOR I = 1 TO NO: OB(I) = 0: NEXT I
-
-    def txpack(self):
+    def txpack(self, node_number, message_type, message_length):
         """
         SUB TXPACK()
         Transmit packet from PC to NODE 
         Use with SUSIC and SMINI nodes """
 
         # FORM PACKET TO SEND TO USIC, SUSIC OR SMINI
-        self.tx_byte[1] = 255              #Set 1st start byte to all 1's
-        self.tx_byte[2] = 255 #TB(2) = 255              #Set 2nd start byte to all 1's
-        self.tx_byte[3] = 2 #TB(3) = 2                #Define start-of-text (STX = 2)
-        self.tx_byte[4] = self.ua + 65 #TB(4) = UA + 65          #Add 65 offset to USIC address
-        self.tx_byte[5] = self.mt #TB(5) = MT               #Define message type
+        self.tx_byte[1] = 255               #Set 1st start byte to all 1's
+        self.tx_byte[2] = 255               #Set 2nd start byte to all 1's
+        self.tx_byte[3] = 2                  #Define start-of-text (STX = 2)
+        self.tx_byte[4] = node_number + 65  #Add 65 offset to NODE address
+        self.tx_byte[5] = message_type      #Define message type
         tp = 6                   #Define next position for transmit pointer
         if self.mt != 80:
             #goto ENDMSG   #Poll request so branch to end message
             i = 1
-            while i <= self.lm:
+            while i <= message_length:
                 # escape control char by prefixing with 16
                 if self.out_byte[i] == 2 | self.out_byte == 3 | self.out_byte == 16:
                     self.tx_byte[tp] = 16

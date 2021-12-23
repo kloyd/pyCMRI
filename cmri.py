7e0f8371900f73427a8dee4eabd4a39ed1152bef
Kelly Loyd <kelly.loyd@icloud.com>
2021-12-23 07:40:33 -0600

    Zero based lists

--- cmri.py
+++ cmri.py
@@ -14,8 +14,12 @@ class CMRI:
         self.init_err = 0
         # REM**GLOBALIZE SERIAL PROTOCOL HANDLING VARIABLES
         #DIM SHARED OB(60), IB(60), CT(15), TB(80), CLOR(170)
-        out_byte = [n for n in range(60)]
-        in_byte = [n for n in range(60)]
+        self.out_byte = []
+        self.in_byte = []
+        for n in range(0, 60):
+            self.out_byte.append(0)
+        for n in range(0, 60):
+            self.in_byte.append(0)
         ct = [n for n in range(15)]
         tx_byte = [n for n in range(80)]
         lm = 0 # length of message
@@ -47,21 +51,24 @@ class CMRI:
         # ns = number of 4 card sets or number of searchlights
         # MAXTRIES = Maximum number of PC tries to read input bytes prior to PC aborting inputs
         # pass
+        for n in range(0,60):
+            self.out_byte[n] = 0
+        
         message_type = ord('I')             # Define message type = "I" (decimal 73)
-        self.out_byte[1] = ord(node_type)   # Define node definition parameter
-        self.out_byte[2] = int(dl / 256)    # Set USIC delay high-order byte
-        self.out_byte[3] = dl % 256         # Set USIC delay low-order byte
-        self.out_byte[4] = ns               #D efine number of card sets of 4 for USIC and SUSIC cases 
+        self.out_byte[0] = ord(node_type)   # Define node definition parameter
+        self.out_byte[1] = int(dl / 256)    # Set USIC delay high-order byte
+        self.out_byte[2] = dl % 256         # Set USIC delay low-order byte
+        self.out_byte[3] = ns               #D efine number of card sets of 4 for USIC and SUSIC cases 
                                             # Or number of 2-lead yellow aspect oscillating signals for SMINI.
 
-        lm = 4  #Initialize length of message to start of loading CT elements
+        lm = 3  #Initialize length of message to start of loading CT elements
 
         # M == SMINI node. X = SUSIC node.
         if node_type == "M":
             # init SMINI - only send CT if NS > 0
             # Always 6 configuration bytes in CT for SMINI
             if ns > 0:
-                for i in range(1, 7):
+                for i in range(0, 6):
                     lm = lm + 1
                     self.out_byte[lm] = ct[i]
         else:
@@ -75,7 +82,7 @@ class CMRI:
             #NEXT I              #...array elements into output byte array
             #GOTO TXMSG  #CT( )s complete so branch to transmit initialization...
                              #...message to interface
-            for i in range(1, ns + 1):
+            for i in range(0, ns):
                 lm = lm + 1
                 self.out_byte[lm] = ct[i]
 
@@ -83,7 +90,7 @@ class CMRI:
         self.txpack(node_number, message_type, message_length=lm)
         # clean up output bytes
         #**COMPLETED USE OF OUTPUT BYTE ARRAY SO CLEAR IT BEFORE EXIT SUBROUTINE
-        for i in range(1, num_out_bytes + 1):
+        for i in range(0, num_out_bytes + 1):
             self.out_byte[i] = 0
 
 
@@ -94,12 +101,12 @@ class CMRI:
         Use with SUSIC and SMINI nodes """
 
         # FORM PACKET TO SEND TO USIC, SUSIC OR SMINI
-        self.tx_byte[1] = 255               #Set 1st start byte to all 1's
-        self.tx_byte[2] = 255               #Set 2nd start byte to all 1's
-        self.tx_byte[3] = 2                  #Define start-of-text (STX = 2)
-        self.tx_byte[4] = node_number + 65  #Add 65 offset to NODE address
-        self.tx_byte[5] = message_type      #Define message type
-        tp = 6                   #Define next position for transmit pointer
+        self.tx_byte[0] = 255               #Set 1st start byte to all 1's
+        self.tx_byte[1] = 255               #Set 2nd start byte to all 1's
+        self.tx_byte[2] = 2                  #Define start-of-text (STX = 2)
+        self.tx_byte[3] = node_number + 65  #Add 65 offset to NODE address
+        self.tx_byte[4] = message_type      #Define message type
+        tp = 5                   #Define next position for transmit pointer
         if self.mt != 80:
             #goto ENDMSG   #Poll request so branch to end message
             i = 1
@@ -115,6 +122,6 @@ class CMRI:
 
         self.tx_byte[tp] = 3 # (TP) = 3  'Add end-of-text (ETX = 3)
 
-        self.serialPort.write(self.tx_byte)
+        # self.serialPort.write(self.tx_byte)
 
   #REM**TRANSMIT PACKET TO USIC, SUSIC OR SMINI
